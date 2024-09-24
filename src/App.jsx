2a83643252f78bcdcecb739b202ecfd399b24258
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-24 12:07:33 -0400

    basic imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import {About, Contact, Experience, Feedbacks, Navbar, Hero, Tech, Works, StarsCanvas } from './components'
 
 const App = () => {
   return (
-    <div>3D Portfolio</div>
+    <div>
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
