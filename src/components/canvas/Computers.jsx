18c01a914c585666a2094adbb5198975fc6b0618
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-25 19:05:49 -0400

    add loading screen

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,15 +4,46 @@ import {OrbitControls, Preload, useGLTF} from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 
-const Computers = () => {
+export const Computers = () => {
 
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF('../../../../public/desktop_pc/scene.gltf')
 
   return (
+
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <ambientLight intensity={1} groundColor="black" />
+      <directionalLight intensity={2} angle={0.12} penumbra={1} castShadow position={[-20, 50, 10]} />
+      <primitive
+      object={computer.scene}
+      scale={0.5}
+      position={[0, -3.25, -1.5]}
+      rotate={[0, -3.25, -1.5]}
+      />
     </mesh>
+
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+   return (
+    <Canvas
+    frameloop='demand'
+    shadows
+    camera={{position: [20,3,5], fov: 25}}
+    gl={{preserveDrawingBuffer: true}}>
+
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+        enableZoom={false}
+        maxPolarAngle={Math.PI/2}
+        minPolarAngle={Math.PI/2}
+        />
+        <Computers/>
+      </Suspense>
+
+    <Preload all />
+    </Canvas>
+   )
+}
+
+export default ComputersCanvas
