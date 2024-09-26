b5ecccbd11158bbab2f53ddd6014ee8768a09b5d
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-26 10:54:37 -0400

    start transition to static website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,9 +16,26 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Marcus</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I build React apps and user interfaces.</p>
         </div>
+      </div>
 
       <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about"></a>
+        <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <motion.div
+          animate={{
+            y: [0, 24, 0]
+          }}
+          transition={{
+            duration: 1.5,
+            repeat: 9999,
+            repeatType: 'loop'
+          }}
+          className="w-3 h-3 rounded-full bg-[#e5e7eb] mb-1"
+          />
+        </div>
       </div>
+
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,12 +1,8 @@
 import React, {Suspense, useEffect, useState} from 'react'
-import {Canvas} from '@react-three/fiber'
-import {OrbitControls, Preload, useGLTF} from '@react-three/drei'
-import CanvasLoader from '../Loader'
 
 
-export const Computers = () => {
+export const Computers = ({isMobile}) => {
 
-  const computer = useGLTF('../../../../public/desktop_pc/scene.gltf')
 
   return (
 
@@ -15,8 +11,8 @@ export const Computers = () => {
       <directionalLight intensity={2} angle={0.12} penumbra={1} castShadow position={[-20, 50, 10]} />
       <primitive
       object={computer.scene}
-      scale={0.5}
-      position={[0, -3.25, -1.5]}
+      scale={isMobile ? 0.1 : 0.5}
+      position={[0, -1.25, -1]}
       rotate={[0, -3.25, -1.5]}
       />
     </mesh>
@@ -25,6 +21,25 @@ export const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(()=> {
+    const mediaQuery = window.matchMedia(
+      ('max-width: 687px'))
+
+      setIsMobile(mediaQuery.matches)
+
+      const handleMediaQueryChange = (event) => {
+        setIsMobile(event.matches);
+      }
+
+      mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+      return () => {
+        mediaQuery.removeEventListener('change', handleMediaQueryChange)
+      }
+  }, []);
+
    return (
     <Canvas
     frameloop='demand'
@@ -35,10 +50,10 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
         enableZoom={false}
-        maxPolarAngle={Math.PI/2}
-        minPolarAngle={Math.PI/2}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
         />
-        <Computers/>
+        <Computers isMobile={isMobile}/>
       </Suspense>
 
     <Preload all />
