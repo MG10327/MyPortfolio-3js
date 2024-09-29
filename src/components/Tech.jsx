42b2967851b0bb217ed191db3973012a7ec79b33
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-29 14:53:41 -0400

    add consistent spacing to top container

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,7 @@ import { technologies } from '../constants'
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10 max-w-[875px] m-auto'>
+    <div className='flex flex-row flex-wrap justify-center gap-10 max-w-[875px] m-auto mt-20'>
       {technologies.map((technology)=> (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
