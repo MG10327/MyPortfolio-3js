31fc5c4bd255af601698d1626ebd1051a13e360b
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-29 19:03:16 -0400

    update job descriptions

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -14,17 +13,16 @@ import reactjs from "./tech/reactjs.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import edreamz from "./company/edreamz-logo.png";
+import rapidcourt from "./company/rapidcourt.webp";
+import codesit from "./company/codesit-logo.webp";
+import criticalmass from "./company/criticalmasslogo.jpg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
-  logo,
   backend,
   creator,
   mobile,
@@ -38,10 +36,10 @@ export {
   reactjs,
   tailwind,
   typescript,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  edreamz,
+  criticalmass,
+  rapidcourt,
+  codesit,
   carrent,
   jobit,
   tripguide,
