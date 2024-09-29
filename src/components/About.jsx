dac355d3e375be929e825991ba3b9769a134f9c9
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-29 15:49:25 -0400

    add project descriptions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
 
       <motion.p
       variants={fadeIn("","",0.1,1)}
-      className=' m-20 mt-4 text-gray-300 text-[17px] max-w-3xl leading-[30px]'>
+      className=' m-20 my-4 text-gray-300 text-[17px] max-w-3xl leading-[30px]'>
         I'm a skilled Front End Developer who works with React, HTML, CSS / SASS, JavaScript, and more. I also build Full stack projects as well. My most notable qualities are my ability to break down problems into small digestible forms and create solutions around them. This often leads to great teamwide documentation, an improved workflow across the board, and ultimately a well crafted website. Let's make high quality work together!
       </motion.p>
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,7 +17,17 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link})=
       className="bg-[#161030] p-5 rounded-2xl sm:w-[360px] w-full">
         <div className="relative w-full h-[230px]">
           <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' />
+          <div className="absolute inset-0 flex justify-end m-4 card-img_hover">
+            <div onClick={()=> window.open(source_code_link, "_blank")} className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
+              <img src={github} alt={github} className='w-1/2 h-1/2 object-contain' />
+            </div>
+          </div>
         </div>
+        
+        <div className='mt-5'>
+            <h3>{name}</h3>
+            <h3>{description}</h3>
+          </div>
       </Tilt>
     </motion.div>
   )
@@ -35,7 +45,7 @@ const Works = () => {
       <div className='w-ful flex'>
         <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-3 text-gray-300 text-[17px] max-w-3xl leading-[30px]'>
+        className=' m-20 my-4 text-gray-300 text-[17px] max-w-3xl leading-[30px]'>
             Each Project that you see here showcases my ability as a developer. The first batch showcases my skills as a Front End Developer, and there are more full stack projects to come.
             Below you will see a link to a live demo for each project and a github link to view the source code, where I solved problems, flexed my skils, and improved my abilities along the way. They are all examples of growth towards the future.
         </motion.p>
