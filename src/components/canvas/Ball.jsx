15bf8599f0c0e64fb857de9ea8145faeeb618443
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-29 14:35:29 -0400

    finish tech balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -7,7 +7,23 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <div></div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0,0,0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1,1]} />
+        <meshStandardMaterial
+        color="#fff8eb"
+        polygonOffset
+        polygonOffsetFactor={-5}
+        flatShading />
+        <Decal 
+        map={decal}
+        position={[0,0,1]}
+        rotation={[2 * Math.PI, 0, 0.625]}
+        flatShading />
+      </mesh>
+    </Float>
   )
 }
 
